848cc71929f637c039fc57f019bc39d85049e2ae
Federico Adreani <fede1397@gmail.com>
2018-09-22 17:08:29 +0200

    Update grepolis.py

--- grepolis.py
+++ grepolis.py
@@ -10,8 +10,8 @@ from selenium.webdriver.chrome.options import Options
 ###############
 #MODIFICA QUESTI PARAMETRI
 ###############
-numero_citta = 30
-citta_riferimento = ""
+numero_citta = 10
+citta_riferimento = "ref"
 username = ""
 password = ""
 
@@ -22,42 +22,48 @@ password = ""
 #INIZIO
 ##############
 
+br = webdriver.Chrome()
+"""
+options = webdriver.ChromeOptions() 
+options.add_argument("user-data-dir=/home/fede3/.config/google-chrome/Default");
+
+br = webdriver.Chrome(chrome_options=options)
+"""
+
 
 nomi_ita = ["senato", "caverna", "legno", "pietra", "argento", "mercato", "porto", "caserma", "muro", "magazzino", "fattoria", "accademia", "tempio", "terme", "torre"]
 nomi = ["main", "hide", "lumber", "stoner", "ironer", "market", "docks", "barraks", "wall", "storage", "farm", "academy", "temple", "terme", "torre"]
 
-matrix_buildings_real = [[0 for i in xrange(15)] for i in xrange(numero_citta)]
+matrix_buildings_real = [[0 for i in range(15)] for i in range(numero_citta)]
 
-legno = 0
-pietra = 0
-argento = 0
-pop = 0
 
 matrix_buildings = [
-	[17, 10, 20, 20, 21, 10, 15, 10, 0, 25, 45, 30, 17, 0, 0],
+	# corretta --> [17, 10, 20, 20, 21, 10, 15, 10, 0, 25, 45, 30, 17, 0, 0],
 
 	#coda bloccata, risparmio energie
 	#[21, 10, 15, 10, 10, 0, 10, 10, 0, 15, 20, 33, 10, 0, 0],#per arrivare alla coloniale piu infretta possibile
 	#[20, 10, 15, 10, 10, 10, 10, 10, 0, 20, 30, 28, 10, 0, 0],#per arrivare alla conquista piu infretta possibile
 
-	[17, 10, 20, 20, 21, 10, 15, 10, 0, 25, 45, 30, 17, 0, 0],
+	#[17, 10, 20, 20, 21, 10, 15, 10, 0, 25, 45, 30, 17, 0, 0],
 
-	[17, 10, 20, 20, 21, 10, 15, 10, 0, 25, 45, 30, 17, 0, 1],
+	#[17, 10, 20, 20, 21, 10, 15, 10, 0, 25, 45, 30, 17, 0, 1],
+	[10, 10, 15, 10, 10, 5, 15, 5, 0, 25, 15, 30, 17, 0, 0],#cresci veloce
+	
 ]
 
 matrix_caserma = [
     [0, 0, 0, 0, 0, 0, 0, 0, 50, 0], # full manticore
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #full mare
-    [500, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 500, 0, 0, 0, 0, 0, 0, 0, 0], #full mare
+    #[500, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
 matrix_porto = [
     [0, 0, 40, 0, 0, 0, 0, 0, 0, 0], # full manticore
-    [0, 0, 330, 0, 0, 0, 0, 0, 0, 0], #full ince
-    [0, 200, 0, 0, 0, 0, 0, 0, 0, 0],
+   # [0, 0, 330, 0, 0, 0, 0, 0, 0, 0], #full ince
+   # [0, 200, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
-err_senato = False
+
 
 
 
@@ -70,9 +76,7 @@ def get_nome_citta(br):
 		name = br.find_element_by_css_selector(".town_name").text
 		return name
 	except:
-		print("impossibile recuperare nome citta")
-		entra_nel_mondo(br)
-
+		login(br)
 
 
 def next_town(br):
@@ -86,49 +90,90 @@ def next_town(br):
 		time.sleep(rand_time())
 	except:
 		print("impossibile cambiare citta")
-		time.sleep(2)
+		login(br)
 
 
 
 def allinea_citta(br):
-	#allinea citta
-	while(True):
-		if(get_nome_citta(br) == citta_riferimento):
-			print("trovata")
-			break
-		else:
-			next_town(br)
-
-def entra_nel_mondo(br):
-	try:
-		search = br.find_elements_by_css_selector(".world_name div")
-		a = 0
-		for i in search:
-			if(a == 2):#NUMERO DEL MONDO CHE COMPARE IN ORDINE NELLA SCHERMATA DOPO LOGIN
-				i.click()
-			a += 1
+	try: 
+		print("allineo")
+		#allinea citta
+		while(True):
+			if(get_nome_citta(br) == citta_riferimento):
+				print("trovata")
+				break
+			else:
+				next_town(br)
+				print("...")
 	except:
-		print("Errore di avvio del gioco..\nprovo ancora")
-		time.sleep(5)
-		entra_nel_mondo(br)
+		print("impossibile allineare citta\nrifaccio login")
+		login(br)
+
+def missioni_iniziali(br):
+	try:
+		search = br.find_element_by_css_selector(".collect_reward")
+		search.click()
+		time.sleep(rand_time())
+		search = br.find_element_by_css_selector(".gp_item_reward_all")
+		search.click()
+		time.sleep(rand_time())
+		search = br.find_element_by_id("item_reward_stash")
+		search.click()
+		print("missione reward depositata")
+		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
+	except:
+		print("visuale reward NON aperta")
+		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()	
+		
+		
+	try:
+		search = br.find_element_by_css_selector(".attack_spot")
+		search.click()
+		print("visuale missione possibile aperta")
+		time.sleep(rand_time())
+		search = br.find_element_by_css_selector(".select_all")
+		search.click()
+		time.sleep(rand_time())
+		search = br.find_element_by_css_selector(".btn_attack")
+		search.click()
+		
+		
+		time.sleep(rand_time())
+		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
+	except:
+		print("visuale missione possibile NON aperta")
+		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
 
 
 def login(br):
 	try:
-		#To fill out a form    ui-button-text
+		br.get("https://it.grepolis.com/")
+		time.sleep(3)
+
 		search = br.find_element_by_name('login[userid]')
 		search.send_keys(username)
 		search = br.find_element_by_name('login[password]')
 		search.send_keys(password)
 		search = br.find_element_by_name("login[Login]")
 		search.send_keys(Keys.RETURN)
-		time.sleep(5)
+		time.sleep(3)
 	except:
-		print("Errore di login..")
-		time.sleep(5)
-		#login(br)
-
-
+		print("login già fatto, entro nel mondo")
+		
+	try:
+		search = br.find_elements_by_css_selector(".world_name div")
+		a = 0
+		for i in search:
+			if(a == 0):#NUMERO DEL MONDO CHE COMPARE IN ORDINE NELLA SCHERMATA DOPO LOGIN
+				i.click()
+			a += 1
+		print("Avvio Gioco")
+	except:
+		print("errore scelta del mondo")
+		
+	time.sleep(3)
+	
+	
 def bonus_giornaliero(br):
 	try:
 		search = br.find_element_by_css_selector(".js-tooltip-resources div")
@@ -138,120 +183,237 @@ def bonus_giornaliero(br):
 		print("Nessun bonus giornaliero")
 
 
-def visuale_citta(br):
+"""
+def missioni_isola(br):
 	try:
-		search = br.find_element_by_css_selector(".city_overview div")
+		search = br.find_elements_by_css_selector(".island_quest")
+		for s in search:
+			s.click()
+			time.sleep(rand_time())
+			decision_containers = br.find_elements_by_css_selector(".decision_container")
+			for dc in decision_containers:
+				if():
+					dc.click()
+		
+		
+		
 		search.click()
-		print("visuale citta")
-		time.sleep(2)
-	except:
-		print("visuale citta non disponibile")
-
-def visuale_isola(br):
-	try:
-		search = br.find_element_by_css_selector(".island_view div")
-		search.click()
-		search = br.find_element_by_css_selector(".btn_jump_to_town div")
-		search.click()
-		print("visuale isola")
-	except:
-		print("visuale isola non disponibile")
-	time.sleep(4)
-
-
-def parametri_citta(br):
-	try:
-		legno = br.find_element_by_css_selector(".ui_resources_bar .wood .amount").text
-		pietra = br.find_element_by_css_selector(".ui_resources_bar .stone .amount").text
-		argento = br.find_element_by_css_selector(".ui_resources_bar .iron .amount").text
-		pop = br.find_element_by_css_selector(".ui_resources_bar .population .amount").text
-		print("parametri citta: " + legno + " "  + pietra + " "  + argento + " "  + pop + " " )
-	except:
-		print("impossibile recuperare parametri citta")
-
-def velocizza_coda_costruzione(br):
-	try:
-		gratis = br.find_element_by_css_selector(".btn_time_reduction")
-		if(gratis.text == "gratis"):
-			gratis.click()
-			print("coda velocizzata")
-			time.sleep(3)
-		else:
-			print("impossibile velocizzare coda")
-	except:
-		print("errore nel velocizzare coda")
-
-def scarsa_popolazione(br):
-	if(int(pop) <= 20):
-		print("Attenzione la popolazione e bassa")
-		br.execute_script("BuildingMain.buildBuilding('farm', 20);")
-
-	time.sleep(2)
-
-def coda_edifici(br):
-	webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
-	try:
-		senato = br.find_element_by_xpath("//*[@id='building_main_area_main']")
-		senato.click()
-		print("visuale senato")
-		err_senato = False
-		time.sleep(2)
-	except:
-		print("visuale senato non disponibile")
-		err_senato = True
-
-
-	if(err_senato==False):
-		edifici = br.find_elements_by_css_selector(".white")
-		cnt_ed = 0
-		for ed in edifici:
-			matrix_buildings_real[n_city][cnt_ed] = int(ed.text)
-			cnt_ed += 1
-
-		print matrix_buildings[n_city]
-		print matrix_buildings_real[n_city]
-
-		for num_edificio in range(0, 13):
-			reale = matrix_buildings_real[n_city][num_edificio]
-			ideale = matrix_buildings[n_city][num_edificio]
-			if(reale < ideale):
-				print("Edificio "+ nomi_ita[num_edificio] +" sottosviluppato di %d livelli" % (reale-ideale))
-				comando_up = "BuildingMain.buildBuilding('"+nomi[num_edificio]+"', 50);"
-				br.execute_script(comando_up)
-	webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
-	print("fine buildings up, wait 5s")
-	time.sleep(5)
-
-def farmer(br):
-	villaggi = br.find_elements_by_xpath("//*[@data-same_island='true']")
-	print("Ho trovato %d villaggi" % len(villaggi))
-	i = 0
-	for villaggio in villaggi:
-		print("agisco sul villaggio " + str(i + 1))
-		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
-		time.sleep(rand_time()+1)
-		search = br.find_elements_by_xpath("//*[@data-same_island='true']") #.owned
-		a = -1
-		for sc in search:
-			a += 1
-			if(a == i):
-				try:
-					sc.click()
-				except:
-					print("impossibile aprire pannello villagio")
-				time.sleep(rand_time())
-				try:
-					ele = br.find_element_by_css_selector(".card_click_area")
-					ele.click()
-				except:
-					print("impossibile raccogliere risorse")
-				time.sleep(rand_time())
-				webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
-		i += 1
 		time.sleep(rand_time())
+		search = br.find_element_by_css_selector(".gp_item_reward_all")
+		search.click()
+		time.sleep(rand_time())
+		search = br.find_element_by_id("item_reward_stash")
+		search.click()
+		print("missione reward depositata")
+		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
+	except:
+		print("visuale reward NON aperta")
+		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
 
 
-def footer(br):
+"""
+
+
+
+
+TEMPO = 5*60 # 5 minuti di tempo refrattario
+
+#start it up
+print("Avvio di chrome....")
+print(rand_time())
+
+login(br)
+bonus_giornaliero(br)
+
+
+
+
+while(True):
+
+	try:
+		search = br.find_elements_by_css_selector(".world_name div")
+		a = 0
+		for i in search:
+			if(a == 2):
+				i.click()
+				print("Avvio Gioco")
+				time.sleep(10)
+			a += 1
+	except:
+		print("gia all'interno del gioco")
+
+
+
+	print("allineo")
+	#allinea citta
+	while(True):
+		if(get_nome_citta(br) == citta_riferimento):
+			print("trovata")
+			break
+		else:
+			next_town(br)
+			print("...")
+
+	start = time.time()
+	for n_city in range(0, numero_citta):
+		time.sleep(2)
+		print(get_nome_citta(br))
+
+		try:
+			search = br.find_element_by_css_selector(".city_overview div")
+			search.click()
+			print("visuale citta")
+			time.sleep(2)
+		except:
+			print("visuale citta non disponibile")
+
+
+		try:
+			legno = br.find_element_by_css_selector(".ui_resources_bar .wood .amount").text
+			pietra = br.find_element_by_css_selector(".ui_resources_bar .stone .amount").text
+			argento = br.find_element_by_css_selector(".ui_resources_bar .iron .amount").text
+			pop = br.find_element_by_css_selector(".ui_resources_bar .population .amount").text
+			print("parametri citta: " + legno + " "  + pietra + " "  + argento + " "  + pop + " " )
+		except:
+			print("impossibile recuperare parametri citta")
+
+
+		try:
+			gratis = br.find_element_by_css_selector(".btn_time_reduction")
+			if(gratis.text == "gratis"):
+				gratis.click()
+				print("coda velocizzata")
+				time.sleep(2)
+			else:
+				print("impossibile velocizzare coda")
+		except:
+			print("errore nel velocizzare coda")
+
+		comando_fattoria_up_dato = False
+		
+		if(int(pop) <= 20):
+			print("Attenzione la popolazione e bassa")
+			try:
+				br.execute_script("BuildingMain.buildBuilding('farm', 20);")
+				print("Comando fattoria dato correttamente")
+				comando_fattoria_up_dato = True
+			except: 
+				print("Errore comando fattoria pop bassa")
+		else:
+			comando_fattoria_up_dato = False
+
+		time.sleep(2)
+		err_senato = False
+		try:
+			#senato = br.find_element_by_id("building_main_area_main")#xpath("//*[@id='building_main_area_main']")
+			#senato.click()
+			br.execute_script("BuildingWindowFactory.open('main');")
+			print("visuale senato")
+			err_senato = False
+			time.sleep(2)
+		except:
+			print("visuale senato non disponibile")
+			err_senato = True
+
+
+		if(err_senato==False):
+			edifici = br.find_elements_by_css_selector(".white")
+			print("edifici ")
+			#print edifici
+
+			cnt_ed = 0
+			for ed in edifici:
+				matrix_buildings_real[n_city][cnt_ed] = int(ed.text)
+				cnt_ed += 1
+
+
+			print (matrix_buildings[n_city])
+			print (matrix_buildings_real[n_city])
+
+			for num_edificio in range(0, 13):
+				if(not(comando_fattoria_up_dato == True and num_edificio == 10)):
+					if(matrix_buildings_real[n_city][num_edificio] < matrix_buildings[0][num_edificio]):
+						print("Edificio "+ nomi_ita[num_edificio] +" sottosviluppato")
+						comando_up = "BuildingMain.buildBuilding('"+nomi[num_edificio]+"', 50);"
+						try:
+							br.execute_script(comando_up)
+						except:
+							print("Errore comando up")
+						#print("Sviluppo edificio "+ nomi[num_edificio] +" sottosviluppato")
+
+
+
+		webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
+
+
+		print("fine buildings up, wait 5s")
+		time.sleep(5)
+
+		try:
+			search = br.find_element_by_css_selector(".island_view div")
+			search.click()
+			search = br.find_element_by_css_selector(".btn_jump_to_town div")
+			search.click()
+			print("visuale isola")
+		except:
+			print("visuale isola non disponibile")
+
+		time.sleep(4)
+		
+		missioni_iniziali(br)
+	#	missioni_isola(br)
+
+		search = br.find_elements_by_xpath("//*[@data-same_island='true']")
+		print(len(search))
+		i = 0
+
+		for sc in search:
+
+			print("agisco sul villaggio " + str(i + 1))
+
+			time.sleep(rand_time()+1)
+
+			search = br.find_elements_by_xpath("//*[@data-same_island='true']") #.owned
+
+
+			a = -1
+			for sc in search:
+				a += 1
+				if(a == i):
+					try:
+						sc.click()
+					except:
+						print("impossibile aprire pannello villagio")
+
+					time.sleep(rand_time())
+
+
+					try:
+						ele = br.find_element_by_css_selector(".card_click_area")
+						ele.click()
+					except:
+						print("impossibile raccogliere risorse")
+
+
+					time.sleep(rand_time())
+
+
+					'''try:
+						ele = br.find_element_by_css_selector(".buttons_container .close")
+						ele.click()
+					except:
+						print("impossibile chiudere")
+					'''
+					webdriver.ActionChains(br).send_keys(Keys.ESCAPE).perform()
+
+			i += 1
+			time.sleep(rand_time())
+			
+
+        # prox citta
+		next_town(br)
+
+
 	end = time.time()
 	print("tempo impiegato: " + str(int(end - start)))
 
@@ -271,68 +433,4 @@ def footer(br):
 			sys.stdout.write("\033[F") # Cursor up one line
 
 		time.sleep(1)
-	print("-------------------------------------------------------------\n-------------------------------------------------------------\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-TEMPO = 600    # 5 minuti di tempo refrattario
-br = webdriver.Chrome()
-
-
-
-#options = webdriver.ChromeOptions()
-#options.add_argument("user-data-dir=/home/kinder/.config/google-chrome") #Path to your chrome profile
-#br = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=options)
-
-
-
-
-#start it up
-print("Avvio di chrome....")
-br.get("https://it.grepolis.com/")
-time.sleep(3)
-
-
-login(br)
-entra_nel_mondo(br)
-bonus_giornaliero(br)
-
-while(True):
-	try:
-		entra_nel_mondo(br)
-		allinea_citta(br)
-		start = time.time()
-
-		for n_city in range(0, numero_citta):
-
-
-			print("Lavoro nella citta: "+get_nome_citta(br))
-
-			visuale_citta(br)
-			parametri_citta(br)
-			velocizza_coda_costruzione(br)
-			scarsa_popolazione(br)
-			coda_edifici(br)
-
-			visuale_isola(br)
-			farmer(br)
-
-			next_town(br)
-
-
-		footer(br)
-	except:
-		print("ERRORE BOT")
-		login(br)
-		time.sleep(30)
+	print("-------------------------------------------------------------\n\n----------------------------------------------------------")
